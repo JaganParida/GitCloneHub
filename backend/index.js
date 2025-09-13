908ee6c375a9760152e073da02e3f35c1daa0ac2
Jagan Parida <jaganparida39064@gmail.com>
2025-09-13 23:42:54 +0530

    Reading terminal command

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const { initRepo } = require("./controllers/init");
+const { addRepo } = require("./controllers/add");
+
+yargs(hideBin(process.argv))
+  .command("init", "Initialize a new repository", {}, initRepo)
+  .command(
+    "add <file>",
+    "Add a file to the repository",
+    (yargs) => {
+      yargs.positional("file", {
+        describe: "File to add to the staging area",
+        type: "String",
+      });
+    },
+    addRepo
+  )
+  .demandCommand(1, "You need atleast one command")
+  .help().argv;
