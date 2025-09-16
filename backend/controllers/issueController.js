f6bb6839fdeb039e50bc69f72d06a6320cf36010
Jagan Parida <jaganparida39064@gmail.com>
2025-09-17 00:48:17 +0530

    Add issue router and controller

---
+++ backend/controllers/issueController.js
@@ -0,0 +1,27 @@
+const createIssue = (req, res) => {
+  res.send("Issue created!");
+};
+
+const updateIssueById = (req, res) => {
+  res.send("Issue updated!");
+};
+
+const deleteIssueById = (req, res) => {
+  res.send("Issue deleted!");
+};
+
+const getAllIssues = (req, res) => {
+  res.send("All Issues Fetched!");
+};
+
+const getIssueById = (req, res) => {
+  res.send("Issue Details Fetched !");
+};
+
+module.exports = {
+  createIssue,
+  updateIssueById,
+  deleteIssueById,
+  getAllIssues,
+  getIssueById,
+};
