909ef055a849ab5eaaf8d4fc835c7e0aea112ff8
Jagan Parida <jaganparida39064@gmail.com>
2025-09-21 20:04:54 +0530

    Add dashboard-1

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav style={{ background: "#161a23", color: "#f1f6fd", padding: "1rem" }}>
+      <h2>GitCloneHub Navbar</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./dashboard.css";
+import Navbar from "../Navbar";
 
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  const [repositories, setRepositories] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestedRepositories, setSuggestedRepositories] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+
+    const fetchRepositories = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3002/repo/user/${userId}`
+        );
+        const data = await response.json();
+        setRepositories(data.repositories);
+      } catch (err) {
+        console.error("Error while fecthing repositories: ", err);
+      }
+    };
+
+    const fetchSuggestedRepositories = async () => {
+      try {
+        const response = await fetch(`http://localhost:3002/repo/all`);
+        const data = await response.json();
+        setSuggestedRepositories(data);
+        console.log(suggestedRepositories);
+      } catch (err) {
+        console.error("Error while fecthing repositories: ", err);
+      }
+    };
+
+    fetchRepositories();
+    fetchSuggestedRepositories();
+  }, []);
 };
 
 export default Dashboard;
