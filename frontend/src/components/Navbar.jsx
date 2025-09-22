c0eb0d5a9391bc75d756c320b7b82b1347c792f1
Jagan Parida <jaganparida39064@gmail.com>
2025-09-22 11:19:01 +0530

    Update in navabr

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <nav style={{ background: "#161a23", color: "#f1f6fd", padding: "1rem" }}>
-      <h2>GitCloneHub Navbar</h2>
+    <nav>
+      <Link to="/">
+        <div>
+          <img
+            src="https://www.github.com/images/modules/logos_page/GitHub-Mark.png"
+            alt="GitHub Logo"
+          />
+          <h3>GitHub</h3>
+        </div>
+      </Link>
+      <div>
+        <Link to="/create">
+          <p>Create a Repository</p>
+        </Link>
+        <Link to="/profile">
+          <p>Profile</p>
+        </Link>
+      </div>
     </nav>
   );
 };
